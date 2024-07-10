38a66a753fc1bc92969fcbc2d5bdb79534693f3d
Alex Golubev <alexandergolubev13@gmail.com>
2024-07-10 10:06:05 +0000

    remove errors about multi gpu support

--- unsloth/models/llama.py
+++ unsloth/models/llama.py
@@ -1239,18 +1239,12 @@ class FastLlamaModel:
 
         # Patch Trainer
         from transformers.trainer import Trainer
-        try:
-            if Trainer._inner_training_loop.__name__ != "_fast_inner_training_loop":
-                inner_training_loop = inspect.getsource(Trainer._inner_training_loop)
-                Trainer._original_training_loop = inner_training_loop
-            else:
-                inner_training_loop = Trainer._original_training_loop
-        except:
-            raise RuntimeError('Unsloth currently does not support multi GPU setups - but we are working on it!')
+        if Trainer._inner_training_loop.__name__ != "_fast_inner_training_loop":
+            inner_training_loop = inspect.getsource(Trainer._inner_training_loop)
+            Trainer._original_training_loop = inner_training_loop
+        else:
+            inner_training_loop = Trainer._original_training_loop
         pass
-
-        if ((post_check - pre_check) >= 1).sum() > 1:
-            raise RuntimeError('Unsloth currently does not support multi GPU setups - but we are working on it!')
 
         import transformers.trainer
         items_in_trainer = dir(transformers.trainer)
@@ -1284,8 +1278,6 @@ class FastLlamaModel:
         except:
             if not torch.cuda.is_available():
                 raise RuntimeError('Unsloth: We do not support AMD / Intel machines yet - it is a work in progress!')
-        if ((a - PRE_CHECK) >= 1).sum() > 1:
-            raise RuntimeError('Unsloth currently does not support multi GPU setups - but we are working on it!')
         for _ in range(3):
             gc.collect()
             torch.cuda.empty_cache()"""
@@ -1345,8 +1337,6 @@ class FastLlamaModel:
             "is_torch_tpu_available()",
             "False",
         )
-        if "n_total_devices >" not in inner_training_loop:
-            raise RuntimeError('Unsloth currently does not support multi GPU setups - but we are working on it!')
         pass
         inner_training_loop = inner_training_loop.replace(
             "is_sagemaker_mp_enabled()",
